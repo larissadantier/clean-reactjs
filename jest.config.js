41e91eee8ea71716582f92922c9b321a85ec6453
larissadantier <larissa_dantier@hotmail.com>
2022-08-04 15:19:52 -0300

    chore: config jest and tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
-  ],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+module.exports = { roots: ['<rootDir>/src'], collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'], coverageDirectory: 'coverage', testEnvironment: 'node', transform: { '.+\\.ts$': 'ts-jest' } }
